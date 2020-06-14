6faecee552b43b7d8ae3f4bd177198b34d8a293a
Robert Quintana <robertquintanatx@gmail.com>
2020-06-14 04:10:25 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,13 @@
 class Post 
-  attr_accessor :author 
+  attr_accessor :author, :title 
   @@all = []
   
+  def initialize(title)
+    @title = title
+  end 
   
+  def self.all 
+    @@all  
+  end 
 
 end 
