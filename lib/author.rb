3935b33cd70511276c7a91b55ffe5deb71c30962
Robert Quintana <robertquintanatx@gmail.com>
2020-06-14 04:15:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,12 +9,16 @@ class Author
     Post.all.select {|post| post.author == self}
   end 
   
-  def add_posts(post)
+  def add_post(post)
     post.author = self
   end 
   
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
+  end 
+  
+  def self.post_count
+    Post.all
   end 
 end 
